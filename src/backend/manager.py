d8080a6b8603ba4f05f451836ad47b0f18ae624d
Mark Gaballa <mgaballa@csu.fullerton.edu>
2025-02-27 15:13:09 -0800

    finish radix

--- src/backend/manager.py
+++ src/backend/manager.py
@@ -6,6 +6,7 @@ import radixSort
 
 test = [1,5,62,7,3,2,6,7,84,2,6126,236,6,36,26362,62,62,36]
 
-val = selectionSort.get_time(test)
+val = radixSort.get_time(test)
+valformated = f"{val:.6f}"
 
-print(val)
+print(valformated)
